4ce3b578aefd13285e36c02972fed122000f4545
Julian Krispel-Samsel <julian@jkrsp.com>
2022-11-24 10:38:09 +0000

    initial start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-
-const Logo = () => {
-  return <span className='text-lg'>Super<span className='font-bold'>Defi</span></span>
-}
+import { Logo } from "../components/Logo";
 
 export default function Home() {
   return (
-    <main className="h-full w-full relative bg-red-200 ">
-      <nav><Logo /></nav>
+    <main className="h-full w-full relative bg-blue-200 ">
+      <nav>
+        <Logo />
+      </nav>
     </main>
-  )
+  );
 }
