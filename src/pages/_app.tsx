0c54753b61fb0eb98c190edf30a541d8e31cbddc
Ashis Kumar <ashishsahoo389@gmail.com>
2023-04-19 21:23:32 +0530

    Chakra-UI setup completed.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
-import { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes';
-import '../styles/globals.css'
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-    <Component {...pageProps} />
-  </ThemeProvider>
-)
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
